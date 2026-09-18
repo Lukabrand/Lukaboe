893348996bb5191d898ddef8456c88c5ea249b80
Lukabrand <lukabrand255@gmail.com>
2026-09-18 02:01:50 -0700

    Update import paths in restrictions.js

--- lukah/restrictions.js
+++ lukah/restrictions.js
@@ -1,7 +1,7 @@
 
-const { gmd } = require("../guru");
-const { getGroupSetting, setGroupSetting } = require("../guru/database/groupSettings");
-const { getSetting, setSetting } = require("../guru/database/settings");
+const { gmd } = require("../luka");
+const { getGroupSetting, setGroupSetting } = require("../luka/database/groupSettings");
+const { getSetting, setSetting } = require("../luka/database/settings");
 
 const OWNER_ONLY = true;
 const ADMIN_ONLY = true;
