3d850f5da603d09f2c13961c4de1dfceeb1c1d91
Lukabrand <lukabrand255@gmail.com>
2026-09-18 13:15:48 +0300

    Update footer text in design.js

--- lukah/design.js
+++ lukah/design.js
@@ -1,4 +1,4 @@
-/**
+ɪᴛ*
  * design.js — Simple Premium Menu System
  */
 
@@ -125,7 +125,7 @@ async function getData(conText) {
 
         footer:
             botFooter ||
-            "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴜᴋᴀ ɪᴛ",
+            "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴜᴋᴀʙʀᴀɴᴅ",
 
         commands:
             totalCommands,
